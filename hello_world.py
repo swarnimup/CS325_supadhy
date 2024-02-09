d142c1844d6d9e5e25a8a26864775c9b0f3d4d5c
Swarnim Upadhyay <supadhy@siue.edu>
2024-02-09 15:01:55 -0600

    Python program

---
+++ hello_world.py
@@ -0,0 +1,8 @@
+name = input("Enter your name: ")
+
+age = input("Enter your age: ")
+
+current_year = 2024
+birth_year = current_year - int(age)
+
+print(f"Hello, {name}! You were born in the year {birth_year}.")
